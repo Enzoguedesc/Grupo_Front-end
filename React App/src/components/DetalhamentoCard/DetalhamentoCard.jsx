ecbc336d2cb0490507824b060dd3ab804fbd5f9b
Diego <diegogode.bonani@gmail.com>
2024-10-24 11:54:19 -0300

    imagens

--- React App/src/components/DetalhamentoCard/DetalhamentoCard.jsx
+++ React App/src/components/DetalhamentoCard/DetalhamentoCard.jsx
@@ -4,7 +4,7 @@ const DetalhamentoCard = (props) => {
     return(
         <DetalheArticle>
             <figure>
-                <img src="https://images.unsplash.com/photo-1516101922849-2bf0be616449?q=80&w=2128&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="imagem"/>
+                <img src={props.url} alt="imagem"/>
             </figure>
             <h1>{props.titulo}</h1>
             <h2>Descrição</h2>
