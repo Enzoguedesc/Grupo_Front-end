5f156aa7aab2f273ff25861d9a01b11cf53d80d4
LucaConfente <luca.bconfente@gmail.com>
2024-11-05 22:24:38 -0300

    idioma 1.0

--- React App/src/components/Header/Header.jsx
+++ React App/src/components/Header/Header.jsx
@@ -6,7 +6,8 @@ const Header = () => (
     <div className="Botoes">
       <img src="https://cdn.portal.estacio.br/Ibmec_logo_branco_97adecdb45.svg" alt='Logomarca-ibmec'/>
       <Link to="/">Home</Link>
-      <Link to="/sobre-nos">Sobre-Nós</Link>
+      <Link to="/">Sobre-Nós</Link>
+      <Link to="/">Idioma</Link>
     </div>
   </Top>
 )
