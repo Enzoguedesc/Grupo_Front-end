982bc9d3267e9780385643e8509d324b445f9bc2
Enzo Guedes <enzoguedesc@gmail.com>
2024-11-06 21:14:58 -0300

    oi

--- React App/src/components/ProjetoCard/Style.js
+++ React App/src/components/ProjetoCard/Style.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const Imagem = styled.article`
+const Cartao = styled.article`
     width: 35vmax;
     background-color: var(--terciaria);
     padding: .5em;
@@ -23,7 +23,7 @@ const Imagem = styled.article`
     .ContainerDetalhes {
       display: grid;
       grid-template-columns: 3fr 1fr;
-
+      font-size: x-small;
     }
 
     figure {
@@ -45,15 +45,20 @@ const Imagem = styled.article`
       background-color: var(--background);
       text-decoration: none;
       font-weight: bold;
-      color: red;
-      border-radius: 5px;
+      color: var(--detalhe);
       text-transform: uppercase;
       /*border-radius: .3em;*/
       &:hover{
         background-color: var(--destaque);
         color: var(--secundaria);
       }
+  }
+
+  @media screen and (min-width: 800px) {
+    .ContainerDetalhes{
+      font-size: large;
     }
+  }
 `;
 
-export {Imagem};
+export {Cartao};
